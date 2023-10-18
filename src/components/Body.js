c1f2093fca9a4c8ce47dffad71d8230ac7e229d3
Bharath <bharathmbhatta11@gmail.com>
2023-10-18 14:41:07 +0530

    Assignment 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,21 @@
 import RestautantCard from "./RestautantCard";
 import ResList from "../../jsonConfig";
+import { useState } from "react";
 
 const Body = () => {
+  const[restrautantList,setrestrautantList]=useState(ResList);
+  const handleTopratedClick =() =>{
+    let filteredList = restrautantList.filter(res=> res.info.avgRatingString >4)
+    setrestrautantList(filteredList)
+  }
   return (
     <div className="body">
-      <div className="searchbar">Search</div>
+      {/* <div className="searchbar">Search</div> */}
+      <div className="top-res">
+        <button onClick={handleTopratedClick}>Top Rated Restrautant</button>
+      </div>
       <div className="res-container">
-        {ResList.map((res) => {
+        {restrautantList.map((res) => {
           return <RestautantCard key={res.info.id} resData={res} />;
         })}
       </div>
